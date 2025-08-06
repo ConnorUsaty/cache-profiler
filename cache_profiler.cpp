7ed44aa6cda13356fafd8676b179c3d01fa1d077
ConnorUsaty <connorusaty@gmail.com>
2025-08-05 22:17:23 -0700

    fix: many small issues - still not getting accurate results

--- cache_profiler.cpp
+++ cache_profiler.cpp
@@ -4,10 +4,10 @@
 #include <iostream>
 #include <random>
 
-std::vector<size_t> gen_rand_idx(size_t cache_sz){
+std::vector<size_t> gen_rand_idx(size_t n_cachelines){
     std::vector<size_t> idx{};
-    idx.reserve(cache_sz/64U);
-    for(size_t i=0; i<cache_sz; i+=64U){
+    idx.reserve(n_cachelines);
+    for(size_t i=0; i<n_cachelines; ++i){
         idx.emplace_back(i);
     }
     
@@ -21,34 +21,49 @@ std::vector<size_t> gen_rand_idx(size_t cache_sz){
 
 int main(){
     size_t constexpr CACHELINE_SIZE = std::hardware_destructive_interference_size;
-    size_t constexpr BUF_SZ = (1U << 18U);
-    size_t constexpr n_iterations = 10000U;
-
+    size_t constexpr BUF_SZ = (1U << 31U);
+    
+    std::vector<std::pair<size_t,long long>> measurements; // {cache_sz, access time (ns)}
     uint8_t* buffer = reinterpret_cast<uint8_t*>(new uint8_t[BUF_SZ]);
 
-    std::vector<std::pair<size_t,long long>> measurements; // {cache_sz, access time (ns)}
-
     for(size_t curr_sz=64U; curr_sz<=BUF_SZ; curr_sz <<= 1U){
-        auto rand_idxs = gen_rand_idx(curr_sz);
+        size_t n_cachelines = curr_sz / CACHELINE_SIZE;
         
-        // warm cache
+        // warm cache to get desired memory layout
+        // memory layout should be like this with lower mem address on left:
+        // RAM ... | L3 ... | L2 ... | L1 ...
         for(size_t i=0; i<curr_sz; i += CACHELINE_SIZE){
             buffer[i] ^= 0x0101;
             volatile uint8_t v = buffer[i];
             (void)v;
         }
 
+        // group in batches for more accurate results
+        size_t batch_sz = 1000U;
+        size_t n_batches = n_cachelines / batch_sz;
+        
+        // start from right (L1)
+        size_t buf_idx = curr_sz-1;
+        
         // time memory accesses
-        auto start = std::chrono::high_resolution_clock::now();
-        for(size_t i=0; i<curr_sz/64U; ++i){
-            size_t idx = rand_idxs[i];
-            volatile uint8_t v = buffer[idx];
-            (void)v;
+        for(size_t batch=0; batch < n_batches; ++batch){
+            auto rand_indexes = gen_rand_idx(batch_sz);
+
+            auto start = std::chrono::high_resolution_clock::now();
+            for(size_t i=0; i < batch_sz; ++i){
+                size_t idx = rand_indexes[i];
+                volatile uint8_t v = buffer[buf_idx - (idx * CACHELINE_SIZE)];
+                (void)v;
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+            buf_idx -= (batch_sz * CACHELINE_SIZE);
+            measurements.push_back({buf_idx * CACHELINE_SIZE, elapsed});
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / (long long)rand_idxs.size();
-        measurements.push_back({curr_sz, elapsed});
     }
+    
+    delete[] buffer;
 
     // dump results to log file
     for(auto& [sz, time] : measurements){
